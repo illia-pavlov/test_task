e7884aca3a1519c4bcae7cebe4c6cc5a3e8c6d25
illia-pavlov <illia.pavlov.qa@gmail.com>
2021-09-23 18:43:47 +0300

    add-tests

--- main.py
+++ main.py
@@ -33,29 +33,38 @@ create_session_data_json = create_session_data
 token = "7464a935a833b06c98accde18a24f378"
 headers = {'Authorization': f'Token token="{token}"'}
 
-if __name__ == '__main__':
+
+def email_login_check(act_data, expec_data):
+    print(act_data)
+    print(expec_data)
+    assert act_data == expec_data, "Login or email is not correct"
+
+
+def create_user():
     create_user_request = requests.post(f'{BASE_URL}users',
                                         json=create_user_data_json,
                                         headers=headers)
-
     user_token = create_user_request.json()['User-Token']
     headers_session_create = {'Authorization': f'Token token="{token}"',
-                              'User-Token': f'{user_token}'}
-
+                              'User-Token': f'{user_token}'
+                              }
     post_session_create = requests.post(f'{BASE_URL}session',
                                         json=create_session_data_json,
                                         headers=headers_session_create)
-
     session_token = post_session_create.json()['User-Token']
+    global headers_get_user
     headers_get_user = {'Authorization': f'Token token="{token}"',
                         'User-Token': f'{session_token}'}
-
     get_user_request = requests.get(f'{BASE_URL}users/{create_user_data_json["user"]["login"]}',
                                     headers=headers_get_user)
+    return get_user_request
 
-    update_user_request = requests.put(f'{BASE_URL}users/{create_session_data_json["user"]["login"]}',
+
+def update_user():
+    url = f'{BASE_URL}users/{create_session_data_json["user"]["login"]}'
+    update_user_request = requests.put(url,
                                        json=update_user_data_json,
                                        headers=headers_get_user)
-
-    get_user_request_upd = requests.get(f'{BASE_URL}users/{update_user_data_json["user"]["login"]}',
-                                        headers=headers_get_user)
+    get_user_request = requests.get(f'{BASE_URL}users/{update_user_data_json["user"]["login"]}',
+                                    headers=headers_get_user)
+    return get_user_request
---
+++ test/test_api.py
@@ -0,0 +1,31 @@
+from main import create_user, update_user, create_user_data_json, update_user_data_json
+
+def test_email_login_check():
+    get_user_request = create_user()
+    actual_login = get_user_request.json()['login']
+    actual_email = get_user_request.json()['account_details']['email']
+    actual_user_data_json = {
+        "user": {
+            "login": f'{actual_login}',
+            "email": f'{actual_email}',
+            "password": "Pa55word"
+        }
+    }
+    print(actual_user_data_json)
+    print(create_user_data_json)
+    assert actual_user_data_json == create_user_data_json, "Login or email is not correct"
+
+
+def test_email_login_after_update_check():
+    get_user_request = update_user()
+    actual_login = get_user_request.json()['login']
+    actual_email = get_user_request.json()['account_details']['email']
+    actual_user_data_json = {
+        "user": {
+            "login": f'{actual_login}',
+            "email": f'{actual_email}'
+        }
+    }
+    print(actual_user_data_json)
+    print(update_user_data_json)
+    assert actual_user_data_json == update_user_data_json, "Login or email is not correct"
